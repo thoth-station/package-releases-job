a06347e843eb355a6fe1e26181a216be5e4806b3
KPostOffice <kpostlet@redhat.com>
2021-01-20 13:17:23 -0500

    update to latest messaging (#530)

--- app.sh
+++ app.sh
@@ -4,20 +4,4 @@
 # command and debug level
 #
 
-set -o nounset
-set -o errexit
-set -o errtrace
-set -o pipefail
-trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
-
-SUBCOMMAND=${SUBCOMMAND:-producer}
-DEBUG_LEVEL=${DEBUG_LEVEL:-0}
-DEBUG_FLAGS=""
-
-if [ "$DEBUG_LEVEL" -eq 1 ]; then
-    DEBUG_FLAGS="--debug --loglevel debug"
-fi
-
-if [ "$SUBCOMMAND" == "producer" ]; then
-    exec faust ${DEBUG_FLAGS} -A producer main
-fi
+exec python producer.py
